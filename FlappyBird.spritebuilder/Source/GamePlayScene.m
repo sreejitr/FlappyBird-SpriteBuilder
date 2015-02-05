f24334053001d68af39711b23e82fb8ad6fbbc5e
sreejitr <sreejitr@andrew.cmu.edu>
2015-02-05 14:53:19 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,7 +17,8 @@
 }
 
 // put new methods here
-- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
     [character flap];
 }
 
